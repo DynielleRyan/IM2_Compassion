6dec00954cf8d541bc712bb74b8cdea0d8b5f8af
KieKeiKeikie <monsiuercat20@gmail.com>
2022-12-11 19:29:04 +0800

    Payment Page Front End

--- Webpages/payment.php
+++ Webpages/payment.php
@@ -5,27 +5,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/payment.css">
     <title>Billing page</title>
 </head>
 <body>
 
-    <form action="../includes/payment.inc.php" method="post">
+    <form  class="card"action="../includes/payment.inc.php" method="post">
         <label>Donation</label>
         <input type="text" name="amount" placeholder="Amount of Donation" required>
-        
         <br><br>
         <label>Personal Message</label>
         <input type="text-area" name ="message"placeholder="Message.....">
         <!-- <input type="hidden" value=""> -->
         <br><br>
-        <button type="submit" name="submit" value="submit">Donate</button>
-    </form>
-            
-    <ul>
+        <button class="button"type="submit" name="submit" value="submit">Donate</button>
+        <ul>
+        <h3>Other options</h3>
         <li><a href="">Gcash</a></li>
         <li><a href="">Paymaya</a></li>
         <li><a href="">Paypal</a></li>
     </ul>
+    </form>
+            
+
 
 
 </body>
