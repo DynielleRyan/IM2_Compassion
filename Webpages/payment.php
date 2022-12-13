38526d3ad23248a8f5d2532deec07f76788a7b0c
KieKeiKeikie <monsiuercat20@gmail.com>
2022-12-13 14:02:45 +0800

    Added Home Button In payment.php

--- Webpages/payment.php
+++ Webpages/payment.php
@@ -5,12 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/payment.css">
     <title>Billing page</title>
 </head>
 <body>
 
     <form  class="card"action="../includes/payment.inc.php" method="post">
+    <a class="btn"href="index.php"><i class="fa fa-home"></i></a>
+    <br>
         <label>Donation</label> 
         <br>
         <input type="number" name="amount" placeholder="Amount of Donation" required>
@@ -18,7 +21,6 @@
         <label>Personal Message</label>
         <br>
         <input type="text-area" name ="message"placeholder="Message.....">
-        <!-- <input type="hidden" value=""> -->
         <br><br>
         <button class="button" type="submit" name="submit" value="submit">Donate</button>
         <br><br><br>
